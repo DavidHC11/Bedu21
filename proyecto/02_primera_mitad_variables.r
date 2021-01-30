735c82c55b179c8e581466b50dc924be647c5139
adrian-cr <acr77frb@gmail.com>
2021-01-30 18:53:14 +0000

    Se renombraron las variables del df data1 Se actualizaron los nombres para las acciones de conformidad Se agregaron comentarios de flujo

--- proyecto/02_primera_mitad_variables.r
+++ proyecto/02_primera_mitad_variables.r
@@ -2,8 +2,15 @@ setwd("./proyecto")
 library(dplyr)
 library(tidyr)
 data <- read.csv('listings.csv', encoding='UTF-8')
-data1 <- data %>% select(id, last_scraped,host_id, host_name, host_since, host_response_time, host_response_rate, host_acceptance_rate, host_is_superhost, host_total_listings_count, host_verifications, host_identity_verified, neighbourhood, neighbourhood_cleansed, neighbourhood_group_cleansed, latitude, longitude, property_type, room_type, accommodates, bathrooms, bathrooms_text, bedrooms, beds,  amenities, price, minimum_nights, maximum_nights, minimum_minimum_nights, maximum_minimum_nights, minimum_maximum_nights, maximum_maximum_nights, minimum_nights_avg_ntm)
-data1 <- data1 %>% mutate(last_scraped = as.Date(last_scraped, "%Y-%m-%d"), host_since = as.Date(host_since, "%Y-%m-%d"), host_since = as.Date(host_since, "%Y-%m-%d"))
-data1 <- data1 %>% mutate(host_response_rate = gsub("\\%", "", host_response_rate), host_acceptance_rate = gsub("\\%", "", host_acceptance_rate), price = gsub("\\$", "", price))
-data1 <- data1 %>% mutate(host_response_rate = as.numeric(host_response_rate)/100, host_acceptance_rate = as.numeric(host_acceptance_rate)/100, price = as.numeric(price))
-data1 <- data1 %>% mutate(host_is_superhost = factor(host_is_superhost, levels=c('t', 'f'), labels=c(1, 0)), host_identity_verified = factor(host_identity_verified, levels=c('t', 'f'), labels=c(1, 0)))
+
+#TIPO DE VARIABLES
+data1 <- data %>% rename(b_id = id, d_last_scraped = last_scraped, b_host_id = host_id, t_host_name = host_name, d_host_since = host_since, b_host_response_time = host_response_time, c_host_response_rate = host_response_rate, c_host_acceptance_rate = host_acceptance_rate, b_host_is_superhost = host_is_superhost, b_host_total_listings_count = host_total_listings_count, t_host_verifications = host_verifications, b_host_identity_verified = host_identity_verified, b_neighbourhood = neighbourhood, b_neighbourhood_cleansed = neighbourhood_cleansed, b_neighbourhood_group_cleansed = neighbourhood_group_cleansed, c_latitude = latitude, c_longitude = longitude, b_property_type = property_type, b_room_type = room_type, b_accommodates = accommodates, b_bathrooms = bathrooms, t_bathrooms_text = bathrooms_text, b_bedrooms = bedrooms, b_beds = beds,  b_amenities = amenities, c_price = price, b_minimum_nights = minimum_nights, b_maximum_nights = maximum_nights, b_minimum_minimum_nights = minimum_minimum_nights, b_maximum_minimum_nights = maximum_minimum_nights, b_minimum_maximum_nights = minimum_maximum_nights, b_maximum_maximum_nights = maximum_maximum_nights, c_minimum_nights_avg_ntm = minimum_nights_avg_ntm)
+
+#CONFORMIDAD
+data1 <- data1 %>% select(b_id, d_last_scraped, b_host_id, t_host_name, d_host_since, b_host_response_time, c_host_response_rate, c_host_acceptance_rate, b_host_is_superhost, b_host_total_listings_count, t_host_verifications, b_host_identity_verified, b_neighbourhood, b_neighbourhood_cleansed, b_neighbourhood_group_cleansed, c_latitude, c_longitude, b_property_type, b_room_type, b_accommodates, b_bathrooms, t_bathrooms_text, b_bedrooms, b_beds,  b_amenities, c_price, b_minimum_nights, b_maximum_nights, b_minimum_minimum_nights, b_maximum_minimum_nights, b_minimum_maximum_nights, b_maximum_maximum_nights, c_minimum_nights_avg_ntm)
+data1 <- data1 %>% mutate(d_last_scraped = as.Date(d_last_scraped, "%Y-%m-%d"), d_host_since = as.Date(d_host_since, "%Y-%m-%d"))
+data1 <- data1 %>% mutate(c_host_response_rate = gsub("\\%", "", c_host_response_rate), c_host_acceptance_rate = gsub("\\%", "", c_host_acceptance_rate), c_price = gsub("\\$", "", c_price))
+data1 <- data1 %>% mutate(c_host_response_rate = as.numeric(c_host_response_rate)/100, c_host_acceptance_rate = as.numeric(c_host_acceptance_rate)/100, c_price = as.numeric(c_price))
+data1 <- data1 %>% mutate(b_host_is_superhost = factor(b_host_is_superhost, levels=c('t', 'f'), labels=c(1, 0)), b_host_identity_verified = factor(b_host_identity_verified, levels=c('t', 'f'), labels=c(1, 0)))
+
+View(data1)
