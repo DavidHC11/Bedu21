07d8cc692afa4de5201336bd6e68fa69772a5574
adrian-cr <acr77frb@gmail.com>
2021-01-31 23:24:02 +0000

    Se agregó función para dar conformidad a variable v_property_type

--- proyecto/limpieza/limpieza.r
+++ proyecto/limpieza/limpieza.r
@@ -2,19 +2,17 @@
 library(dplyr)
 library(tidyr)
 
-
 # ====================== Dependencias locales ========================
 
 # ** Cambiar directorio de trabajo adecuado a ubicación de fuentes.
-
 source("./01_seleccion_variables.r")
 source("./02_etiquetado_variables.r")
 source("./03_eliminacion_duplicados.r")
 source("./04_completitud.r")
 
 # ** Cambiar directorio de trabajo a ubicación de CSV.
-setwd("..")
 
+setwd("..")
 data <- read.csv("listings.csv", encoding = "UTF-8")
 
 df <- seleccionar_variables(data)
@@ -28,7 +26,6 @@ df <- eliminar_nulos(df)
 df <- eliminar_cadenas_no_validas(df)
 
 # ========= Conformidad ==========
-
 # Revisar con hist() o table()
 conformidad_c <- function(df) {
    
@@ -37,11 +34,10 @@ conformidad_t <- function(df) {
    
 }
 conformidad_v <- function(df) {
-   
+  rare_props <- as.vector((as.data.frame(table(df$v_property_type)) %>% filter(Freq <= 10))$Var1)
+  df <- df %>% filter(!(v_property_type %in% rare_props))
+  return(df)
 }
 conformidad_d <- function(df) {
-   
+
 }
-
-
-summary(df)
