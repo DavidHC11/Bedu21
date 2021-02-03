3bc43f9f9c3ed4f7a66762d2a77aa718e3c8a32e
AlanKev117 <fuentes.alan.98@gmail.com>
2021-02-03 00:31:50 -0600

    Datasets completos

--- proyecto/limpieza/limpieza.r
+++ proyecto/limpieza/limpieza.r
@@ -1,12 +1,14 @@
 # ================== Dependencias globales ======================
 
-setwd("proyecto/limpieza")
 library(dplyr)
 library(tidyr)
+
 
 # ====================== Dependencias locales ========================
 
 # ** Cambiar directorio de trabajo adecuado a ubicación de fuentes.
+setwd("proyecto/limpieza")
+
 source("./01_seleccion_variables.r")
 source("./02_etiquetado_variables.r")
 source("./03_eliminacion_duplicados.r")
@@ -14,8 +16,11 @@ source("./04_completitud.r")
 source("./05_conformidad_integridad.r")
 
 # ** Cambiar directorio de trabajo a ubicación de CSV.
-
 setwd("..")
+
+R.utils::gunzip("listings2019.csv", remove = FALSE)
+R.utils::gunzip("listings2020.csv", remove = FALSE)
+
 data2019 <- read.csv("listings2019.csv", encoding = "UTF-8")
 data2020 <- read.csv("listings2020.csv", encoding = "UTF-8")
 
